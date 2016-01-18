7694dc8c39d8442dec141f69ef0c40aa9bfcef5a
Felix Dube <felix.dube@mail.mcgill.ca>
2016-01-18 16:05:22 -0500

    Add stuff for assignment 1

---
+++ Assignment1/heapsStudent.cpp
@@ -0,0 +1,28 @@
+#include "heaps.h"
+
+Heap *heapFromArray(string *input, int length) {
+    //your code here
+}
+
+int numElements(Heap h) {
+    //your code here
+}
+
+size_t lengthOfContent(Heap h) {
+    //your code here
+}
+
+
+/* might want to do this for Q5
+Heap **returnAllHeaps(Heap h) {
+    //your code here
+}*/
+
+string *printLinear(Heap h) {
+    //your code here
+}
+
+
+string printPretty(Heap h) {
+    //your code here
+}
