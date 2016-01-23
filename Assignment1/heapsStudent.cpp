76656106759b79aac041f5988a3fb6cdd34d9ba9
Felix Dube <felix.dube@mail.mcgill.ca>
2016-01-22 22:07:29 -0500

    Fix question 2

--- Assignment1/heapsStudent.cpp
+++ Assignment1/heapsStudent.cpp
@@ -5,43 +5,63 @@ Heap *heapFromArray(string *input, int length) {
 
 	Heap *heaps[length];
 
+    Heap h;
+    heaps[0] = &h;
+
     for(int i = 0; i < length; i++){
     	
     	heaps[i]->name = input[i];
-    	
-    	// check is the heap have children
+
     	if(2*i+2 == length){
-	    	heaps[i]->left = heaps[2*i+1];
-	    	heaps[i]->right = nullptr;
+	     	// create one child
+	     	Heap hl;
+
+	     	// save the address of the child in the array
+	     	heaps[2*i+1] = &hl;
+
+	     	//link child to parent
+	     	heaps[i]->left = &hl;
+	     	heaps[i]->right = nullptr;
     	}
     	else if(2*i+2 > length){
+    		// this node does not have child
 	    	heaps[i]->left = nullptr;
 	    	heaps[i]->right = nullptr;
 	    }
 	    else{
-	    	heaps[i]->left = heaps[2*i+1];
-    		heaps[i]->right = heaps[2*i+2];
-	    }
+	     	// create two child
+	     	Heap hl;
+	     	Heap hr;
 
-	    return heaps[0];
-    }
+	     	// save the address of the child in the array
+	     	heaps[2*i+1] = &hl;
+	     	heaps[2*i+2] = &hr;
+
+	     	//link child to parent
+	     	heaps[i]->left = &hl;
+	     	heaps[i]->right = &hr;
+
+	     }
+	 }
+
+	return heaps[0];
+    
 }
 
 int numElements(Heap h) {
-    
-int num = 0;
+	int num = 1;
 
-    if(h.right != nullptr){
-    	num = num + numElements(*h.right);
-    }
-    else if(h.left != nullptr){
-    	num = num + numElements(*h.left);
-    }
-    else{
-    	return num;
-    }
+	// divide and conquer
+	if(h.right != nullptr){
+		num = num + numElements(*h.left) + numElements(*h.right);
+	}
+	else if(h.left != nullptr){
+		num = num + numElements(*h.left);
+	}
 
+	return num;
 }
+
 
 size_t lengthOfContent(Heap h) {
     //your code here
--- Assignment1/heapsTest.cpp
+++ Assignment1/heapsTest.cpp
@@ -16,33 +16,33 @@ int main() {
     cout << "-------------------------------" << endl;
     cout << numElements(*heapOfBands) << endl;
 
-    // test lenght of content
-    cout << "------------------------------" << endl;
-    cout << "-Testing length of content...-" << endl;
-    cout << "------------------------------" << endl;
-    cout << "Length of content should be 129" << ". My code gives " << lengthOfContent(*heapOfBands) << "." << endl;
+    // // test lenght of content
+    // cout << "------------------------------" << endl;
+    // cout << "-Testing length of content...-" << endl;
+    // cout << "------------------------------" << endl;
+    // cout << "Length of content should be 129" << ". My code gives " << lengthOfContent(*heapOfBands) << "." << endl;
 
-    // test linear print
-    cout << "-------------------------" << endl;
-    cout << "-Testing linear print...-" << endl;
-    cout << "-------------------------" << endl;
-    cout << "It should be: " << endl;
-    for (int i=0; i< numBands; i++) {
-        cout << maBands[i] << " ";
-    }cout << endl;
+    // // test linear print
+    // cout << "-------------------------" << endl;
+    // cout << "-Testing linear print...-" << endl;
+    // cout << "-------------------------" << endl;
+    // cout << "It should be: " << endl;
+    // for (int i=0; i< numBands; i++) {
+    //     cout << maBands[i] << " ";
+    // }cout << endl;
     
-    cout << "Code gives: " << endl;
-    string *pl = printLinear(*heapOfBands);
-    for (int i=0; i<14; i++) {
-        cout << pl[i] << " ";
-    }cout << endl;
+    // cout << "Code gives: " << endl;
+    // string *pl = printLinear(*heapOfBands);
+    // for (int i=0; i<14; i++) {
+    //     cout << pl[i] << " ";
+    // }cout << endl;
     
     
-    // test pretty print
-    cout << "------------------------------" << endl;
-    cout << "-Testing length of content...-" << endl;
-    cout << "------------------------------" << endl;
-    cout << endl << printPretty(*heapOfBands) << endl;
+    // // test pretty print
+    // cout << "------------------------------" << endl;
+    // cout << "-Testing length of content...-" << endl;
+    // cout << "------------------------------" << endl;
+    // cout << endl << printPretty(*heapOfBands) << endl;
 }
 
 
