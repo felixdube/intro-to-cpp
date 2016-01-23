86c6148bbe9e7958fa6541e17000228abb1a885c
Felix Dube <felix.dube@mail.mcgill.ca>
2016-01-22 19:10:42 -0500

    Add question 1-3

--- Assignment1/heapsStudent.cpp
+++ Assignment1/heapsStudent.cpp
@@ -1,11 +1,46 @@
 #include "heaps.h"
 
+
 Heap *heapFromArray(string *input, int length) {
-    //your code here
+
+	Heap *heaps[length];
+
+    for(int i = 0; i < length; i++){
+    	
+    	heaps[i]->name = input[i];
+    	
+    	// check is the heap have children
+    	if(2*i+2 == length){
+	    	heaps[i]->left = heaps[2*i+1];
+	    	heaps[i]->right = nullptr;
+    	}
+    	else if(2*i+2 > length){
+	    	heaps[i]->left = nullptr;
+	    	heaps[i]->right = nullptr;
+	    }
+	    else{
+	    	heaps[i]->left = heaps[2*i+1];
+    		heaps[i]->right = heaps[2*i+2];
+	    }
+
+	    return heaps[0];
+    }
 }
 
 int numElements(Heap h) {
-    //your code here
+    
+int num = 0;
+
+    if(h.right != nullptr){
+    	num = num + numElements(*h.right);
+    }
+    else if(h.left != nullptr){
+    	num = num + numElements(*h.left);
+    }
+    else{
+    	return num;
+    }
+
 }
 
 size_t lengthOfContent(Heap h) {
